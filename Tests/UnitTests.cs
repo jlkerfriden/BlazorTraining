fc3075e6eeb21fbb8835a1bb8da40ffedf9cce64
Jean-Louis Kerfriden <jeanlouis.kerfriden@gmail.com>
2023-02-09 17:27:03 +0100

    Ajoutez des fichiers projet.

---
+++ Tests/UnitTests.cs
@@ -0,0 +1,25 @@
+﻿using BlazorTraining.Pages;
+using Bunit;
+using Xunit;
+
+namespace BlazorTraining.Tests
+{
+    public class UnitTests
+    {
+        [Fact]
+        public void CounterShouldIncrementWhenSelected()
+        {
+            // Arrange
+            using var ctx = new TestContext();
+            var cut = ctx.RenderComponent<Counter>();
+            var paraElm = cut.Find("p");
+
+            // Act
+            cut.Find("button").Click();
+            var paraElmText = paraElm.TextContent;
+
+            // Assert
+            paraElmText.MarkupMatches("Current count: 4");
+        }
+    }
+}
